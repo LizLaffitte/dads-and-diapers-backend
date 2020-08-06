9f41002ac1316b15033eba912c4169d73c99a268
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-06 15:47:44 -0400

    Generated User resource.

--- db/migrate/20200806193604_create_reviews.rb
+++ db/migrate/20200806193604_create_reviews.rb
@@ -2,7 +2,13 @@ class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
       t.string :name
-
+      t.references :listing, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.boolean :mens
+      t.boolean :womens
+      t.boolean :private
+      t.boolean :broken
+      t.integer :clean
       t.timestamps
     end
   end

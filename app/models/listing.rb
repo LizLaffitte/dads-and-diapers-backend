a0435ce99fae6302260cbcfbb97598f7de27c7ca
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-06 16:36:05 -0400

    Git ignore file added. Validate name presence in Listing.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,5 @@ class Listing < ApplicationRecord
     has_many :reviews
     geocoded_by :address, latitude: :lat, longitude: :long
     after_validation :geocode
+    validates :name, presence: true
 end

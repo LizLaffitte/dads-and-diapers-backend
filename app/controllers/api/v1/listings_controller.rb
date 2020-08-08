7c25cb28fe76f02b05eaf173a05378634e35ee70
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-08 12:23:01 -0400

    Helper method find_listing, update action.

--- app/controllers/api/v1/listings_controller.rb
+++ app/controllers/api/v1/listings_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::ListingsController < ApplicationController
+    before_action :find_listing, only: [:show, :update]
     def index
         listings = Listing.all
         render json: listings
     end
 
     def show
-        listing = Listing.find_by_id(params[:id])
-        render json: listing
+        render json: @listing
     end
 
     def create 
@@ -14,15 +14,26 @@ class Api::V1::ListingsController < ApplicationController
         if listing.save
             render json: listing
         else
-            render json: {errors: listing.errors.full_messages},
-            status: unprocessable_entity
+            render json: {errors: listing.errors.full_messages}, status: unprocessable_entity
         end
+    end
+
+    def update
+        @listing.update(listing_params)
+        if @listing.save
+            render json: @listing
+        else
+            render json: {errors: @listing.errors.full_messages}, status: :unprocessable_entity
     end
 
     private
 
     def listing_params
         params.require(:listing).permit(:name, :address)
+    end
+
+    def find_listing
+        @listing = Listing.find_by_id(params[:id])
     end
 
 end

3d9cdb35ddbbd081ae9a1227c0897b94bc1e1271
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-09 16:40:45 -0400

    Moved review routes under api/v1 namespace.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :reviews
+  
   
   namespace :api do
     namespace :v1 do
         resources :listings
         resources :users
+        resources :reviews
     end
   end
 end

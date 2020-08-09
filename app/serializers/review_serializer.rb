8e3cdfa64aa7e5b92e7f46902dcb75434f37246e
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-09 16:47:40 -0400

    Added belongs_to relationship to listing.

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,4 +1,5 @@
 class ReviewSerializer
   include FastJsonapi::ObjectSerializer
-  attributes 
+  attributes :listing_id, :user_id, :mens, :womens, :private, :broken, :clean
+  belongs_to :listing, serializer: ListingSerializer
 end

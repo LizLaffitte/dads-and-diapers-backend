741590803428603b9082146c7de7f6329923ed95
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-09 17:29:17 -0400

    Wrote out sessions#create action.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,12 @@
 class SessionsController < ApplicationController
+
+    def create
+        @user = User.find_by(username: params[:session][:username])
+        if @user && @user.authenticate(params[:session][:password])
+            session[:user_id] = @user.user_id
+            render json: UserSerializer.new(@user)
+        else
+            render json: {errors: @user.errors.full_messages}, status: unprocessable_entity
+        end
+    end
 end

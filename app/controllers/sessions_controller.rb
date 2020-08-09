e55433e26a8836cbb11fa1db6a180b80eb6abdd7
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-09 18:26:18 -0400

    Fixed reference to user id in sessions controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     def create
         @user = User.find_by(username: params[:session][:username])
         if @user && @user.authenticate(params[:session][:password])
-            session[:user_id] = @user.user_id
+            session[:user_id] = @user.id
             render json: UserSerializer.new(@user)
         else
             render json: {errors: @user.errors.full_messages}, status: unprocessable_entity

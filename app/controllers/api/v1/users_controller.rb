5d850fda8e0bfb9a0a8c5c502463583305b941f9
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-10 19:37:38 -0400

    Fix signup route and wrap user_params so that the backend will successfully create and save a new user.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::UsersController < ApplicationController
     before_action :find_user, only: [:show, :update]
-    
+    wrap_parameters :user, include: [:username, :password, :email]
     def create
-        user = User.new(user_params)
-        if user.save
-            session[:user_id] = user.id
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
             render json: UserSerializer.new(@user), status: :created
         else 
-            render json: {error: user.errors.full_messages.to_sentence, status: :unprocessable_entity}
+            render json: {error: @user.errors.full_messages.to_sentence, status: :unprocessable_entity}
         end
 
     end

c9e93899a8e1b99551f4744f39b04e4eb566ab62
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-10 17:31:38 -0400

    Restructure Listings JSON obj.

--- app/serializers/listing_serializer.rb
+++ app/serializers/listing_serializer.rb
@@ -1,5 +1,17 @@
 class ListingSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :address, :lat, :long, :mens_chance, :womens_chance, :private_chance, :clean_chance, :broken_chance
-  has_many :reviews, serlializer: ReviewSerializer
+  attribute :reviews do | listing |
+    listing.reviews.map do | review |
+      {
+        id: review.id,
+        user_id: review.user_id, 
+        mens: review.mens, 
+        womens: review.womens, 
+        private: review.private, 
+        broken: review.broken, 
+        clean: review.clean
+    }
+    end
+  end
 end
--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,5 +1,6 @@
 class ReviewSerializer
   include FastJsonapi::ObjectSerializer
   attributes :listing_id, :user_id, :mens, :womens, :private, :broken, :clean
+  
   belongs_to :listing, serializer: ListingSerializer
 end

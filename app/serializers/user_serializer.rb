be5d295e761e53f803b197cf0dd5dba399833b82
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-10 17:38:43 -0400

    Restructure User JSON object returned by serializer.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,17 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :username
-  has_many :reviews, serializer: ReviewSerializer
-
-  
+  attributes :username, :rank
+  attribute :reviews do | user |
+    user.reviews.map do | review |
+      {
+        id: review.id,
+        location_id: review.listing_id, 
+        mens: review.mens, 
+        womens: review.womens, 
+        private: review.private, 
+        broken: review.broken, 
+        clean: review.clean
+      }
+    end
+  end  
 end

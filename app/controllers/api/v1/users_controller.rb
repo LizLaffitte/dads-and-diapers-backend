578a80dbfcf149ac63483354be6e9e4fef35802c
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-10 17:45:27 -0400

    Create route and action to allow users to signup.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,22 @@
 class Api::V1::UsersController < ApplicationController
     before_action :find_user, only: [:show, :update]
+    
+    def create
+        user = User.new(user_params)
+        if user.save
+            session[:user_id] = user.id
+            render json: UserSerializer.new(@user), status: :created
+        else 
+            render json: {error: user.errors.full_messages.to_sentence, status: :unprocessable_entity}
+        end
+
+    end
+
+    def index
+        users = User.all
+        render json: UserSerializer.new(users)
+    end
+    
     def show
         render json: UserSerializer.new(@user)
     end

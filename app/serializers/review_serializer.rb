13ed69bb410e495872f3cd080fcaab716852a472
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-10 17:36:41 -0400

    Restructure Review JSON object returned by serializer.

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,6 +1,24 @@
 class ReviewSerializer
   include FastJsonapi::ObjectSerializer
   attributes :listing_id, :user_id, :mens, :womens, :private, :broken, :clean
-  
-  belongs_to :listing, serializer: ListingSerializer
+  attribute :listing do | review| 
+    {
+      id: review.listing.id,
+      name: review.listing.name, 
+      address: review.listing.address, 
+      lat: review.listing.lat, 
+      long: review.listing.long, 
+      mens_chance: review.listing.mens_chance, 
+      womens_chance: review.listing.womens_chance, 
+      private_chance: review.listing.private_chance, 
+      clean_chance: review.listing.clean_chance, 
+      broken_chance: review.listing.broken_chance
+    }
+  end
+  attribute :user do |review| 
+    {
+      username: review.user.username,
+      rank: review.user.rank
+    } 
+  end
 end

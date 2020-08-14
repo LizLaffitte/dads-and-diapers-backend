b7695ef772d99c93cbdf0cc2675a9e1fa98bd9ea
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-13 20:57:11 -0400

    Allow backend to create new reviews.in the database.

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ReviewsController < ApplicationController
     before_action :find_review, only: [:show, :update]
-    
+    wrap_parameters :review, include: [:mens, :womens, :private, :broken, :clean, :user_id, :listing_id]
     def index
         reviews = Review.all
         render json: ReviewSerializer.new(reviews)
@@ -8,6 +8,15 @@ class Api::V1::ReviewsController < ApplicationController
 
     def show
         render json: ReviewSerializer.new(@review)
+    end
+
+    def create
+        review = Review.new(review_params)
+        if review.save
+            render json: ReviewSerializer.new(review)
+        else
+            render json: {error: review.errors.full_messages.to_sentence}, status: :unprocessable_entity
+        end
     end
 
     private

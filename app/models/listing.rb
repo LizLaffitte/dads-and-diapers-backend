abebd378398680893d5071874621415354a2fee4
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-15 13:30:44 -0400

    Add instance methods to calculate and update _chance attributes.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,71 @@ class Listing < ApplicationRecord
     geocoded_by :address, latitude: :lat, longitude: :long
     after_validation :geocode
     validates :name, presence: true
+
+    def womens_true
+        trues = 0.0
+        self.reviews.each do | review |
+            if review.womens
+                trues += 1.0
+            end
+        end
+        self.calc_percentages(trues)
+    end
+
+    def mens_true
+        trues = 0.0
+        self.reviews.each do | review |
+            if review.mens
+                trues += 1.0
+            end
+        end
+        self.calc_percentages(trues)
+    end
+
+    def isolated_true
+        trues = 0.0
+        self.reviews.each do | review |
+            if review.isolated
+                trues += 1.0
+            end
+        end
+        self.calc_percentages(trues)
+    end
+
+    def clean_true
+        trues = 0.0
+        self.reviews.each do | review |
+            if review.clean
+                trues += 1.0
+            end
+        end
+        self.calc_percentages(trues)
+    end
+
+    def working_true
+        trues = 0.0
+        self.reviews.each do | review |
+            if review.working
+                trues += 1.0
+            end
+        end
+        self.calc_percentages(trues)
+    end
+
+    def calc_chances
+        self.update(
+            :womens_chance => self.womens_true,
+            :mens_chance => self.mens_true,
+            :clean_chance => self.clean_true,
+            :isolated_chance => self.isolated_true,
+            :working_chance => self.working_true
+            )
+    end
+
+    def calc_percentages(true_total)
+        ( true_total / self.reviews.count) * 100
+    end
+    
+
+
 end

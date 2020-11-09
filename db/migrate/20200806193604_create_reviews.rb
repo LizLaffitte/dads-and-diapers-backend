b3a97c7ff9ce3be01ffe34347c728bdf298c03fc
lizlaffitte <laffitte.digital@gmail.com>
2020-11-09 13:27:40 -0500

    Remove foreign key reference.

--- db/migrate/20200806193604_create_reviews.rb
+++ db/migrate/20200806193604_create_reviews.rb
@@ -2,8 +2,8 @@ class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
       t.string :name
-      t.references :listing, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
+      t.references :listing, null: false, references: :listings
+      t.references :user, null: false, references: :users
       t.boolean :mens
       t.boolean :womens
       t.boolean :private
